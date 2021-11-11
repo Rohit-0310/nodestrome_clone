3b2bf9f9a6081bddfb501059de6dfdddbcfddbf6
Rohit Kumar <rohit9909mr@gmail.com>
2021-11-11 20:19:54 +0530

    update

--- components/navbar.js
+++ components/navbar.js
@@ -7,21 +7,21 @@ function navbar() {
         <div id="options">
             <input type="text" id="Search" placeholder="Search" />
             <button onclick="Search()" id="Search">Search</button>
-            <h3><a href="#">Sign In</a></h3>
+            <h3><a href="Signupandlogin/Sing up.html">Sign In</a></h3>
             <h3><a href="cart.html">Cart</a></h3>
         </div>
     </div>
     
     <div id="navbar_2">
     
-        <div>Black Friday</div>
-        <div>Holiday Gift</div>
+        <div><a href="BlackFriday.html">Black Friday</a></div>
+        <div><a href="holidaygift.html">Holiday Gift</a></div>
         <div><a href="women.html">Women</a></div>
         <div><a href="men.html">Men</a></div>
         <div>Kids</div>
         <div>Young Adults</div>
         <div>Activewear</div>
-        <div>Home</div>
+        <div><a href="index.html">Home</a></div>
         <div>Beauty</div>
         <div>Designer</div>
         <div>Brands</div>
